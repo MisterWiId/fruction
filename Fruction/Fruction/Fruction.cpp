b3895d446987fea7eb4a2a88d68cf74e77327a2d
MisterWiId <134141287+MisterWiId@users.noreply.github.com>
2024-04-15 20:55:55 +0700

    Update Fruction.cpp

--- Fruction/Fruction/Fruction.cpp
+++ Fruction/Fruction/Fruction.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 
 using namespace std;
+/// <summary>
+/// num - Числтель
+/// denum - Знаменатель
+/// </summary>
+class Fraction
+{
+private:
+    int num;   
+    int denum;
+};
 
 int main()
 {
